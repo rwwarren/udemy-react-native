822732692b12e4251b390a732578eaa335542306
Ryan Warren <ryan@war.re>
2017-03-30 22:05:46 -0700

    albums done

---
+++ albums/src/components/Base.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class NAME extends Component {
+
+render() {
+    return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+  }
+}
+
+export default NAME;
+
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const NAME = () => {
+
+    return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+}
+
+export default NAME;
