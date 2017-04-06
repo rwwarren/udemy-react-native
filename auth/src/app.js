281e3f6e642d686f186bf9a0f070b39c55feb20b
Ryan Warren <ryan@war.re>
2017-04-05 21:19:17 -0700

    auth app

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
 
-  render(){
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyACIbgWiFraLxo9bN_D5Y6hthT86oD2k1M',
+      authDomain: 'auth-8adff.firebaseapp.com',
+      databaseURL: 'https://auth-8adff.firebaseio.com',
+      projectId: 'auth-8adff',
+      storageBucket: 'auth-8adff.appspot.com',
+      messagingSenderId: '1010407593011'
+    });
+  }
+
+
+  render() {
     return (
       <View>
-      <Header headerText="Authentication" />
-        <Text>Test</Text>
+        <Header headerText='Authentication' />
+        <LoginForm />
       </View>
     );
   }
