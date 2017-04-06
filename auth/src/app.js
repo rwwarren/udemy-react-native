4e4b73eeea26a1e3df6bef224832d3266cec3ade
Ryan Warren <ryan@war.re>
2017-04-05 20:45:18 -0700

    auth app

---
+++ auth/src/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Header } from './components/common';
+
+class App extends Component {
+
+  render(){
+    return (
+      <View>
+      <Header headerText="Authentication" />
+        <Text>Test</Text>
+      </View>
+    );
+  }
+
+}
+
+export default App;
